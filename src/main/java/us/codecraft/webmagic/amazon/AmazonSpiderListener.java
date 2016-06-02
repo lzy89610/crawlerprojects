cfb1d9fef1eb3ea72769f59987f706458e46aa0c
lizeyu <3177448156@qq.com>
2016-06-02 10:23:46 +0800

    增加listener

---
+++ src/main/java/us/codecraft/webmagic/amazon/AmazonSpiderListener.java
@@ -0,0 +1,21 @@
+package us.codecraft.webmagic.amazon;
+
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.SpiderListener;
+
+/**
+ * Created by lizeyu on 2016/6/2.
+ */
+public class AmazonSpiderListener implements SpiderListener{
+
+
+    @Override
+    public void onSuccess(Request request){
+
+    }
+
+    @Override
+    public void onError(Request request){
+        System.out.println("error");
+    }
+}
