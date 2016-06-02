27314c35619be410d02f7fe1d32434c6571d16e1
lizeyu <3177448156@qq.com>
2016-06-02 11:18:35 +0800

    增加日志

--- src/main/java/us/codecraft/webmagic/amazon/AmazonSpiderListener.java
+++ src/main/java/us/codecraft/webmagic/amazon/AmazonSpiderListener.java
@@ -1,5 +1,8 @@
 package us.codecraft.webmagic.amazon;
 
+import org.apache.http.conn.ConnectTimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.SpiderListener;
 
@@ -8,6 +11,7 @@ import us.codecraft.webmagic.SpiderListener;
  */
 public class AmazonSpiderListener implements SpiderListener{
 
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public void onSuccess(Request request){
@@ -16,7 +20,25 @@ public class AmazonSpiderListener implements SpiderListener{
 
     @Override
     public void onError(Request request, Exception e){
-        e.printStackTrace();
+
+        String statusCode = (String) request.getExtra("statusCode");
+
+        if("404".equals(statusCode)){
+            //todo:
+        }else if("403".equals(statusCode)){
+
+        }else{
+
+        }
+
+        if(e instanceof ConnectTimeoutException){
+
+        }
+
+        if(e instanceof RuntimeException){
+
+        }
+
     }
 
 
